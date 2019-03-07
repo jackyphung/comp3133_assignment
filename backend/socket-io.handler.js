2e025b7481e9018c31a6ec717c69c419f46bb4a9
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-03-06 22:16:36 -0500

    [SocketIO] Implemented working rooms :D

--- backend/socket-io.handler.js
+++ backend/socket-io.handler.js
@@ -5,48 +5,78 @@ module.exports = {
     io.on('connection', (socket) => {
       console.log('New user connected');
 
-      socket.username = "Anonymous";
+      socket.user = {
+        name: "Anonymous",
+        room: "main-room",
+      }
+
+      socket.join(socket.user["room"]);
 
       socket.send('hi');
 
-      //socket.emit('new_connection', { message: " connected", username: socket.username });
-      io.emit('new_connection', { message: " has connected", username: socket.username });
+      //socket.emit('new_connection', { message: " connected", username: socket.user["name"] });
+      io.emit('new_connection', {
+        username: socket.user["name"], 
+        message: " has connected"
+      });
 
       socket.on('change_username', (data) => {
-        socket.username = data.username
+        socket.user["name"] = data.username
       });
 
       socket.on('new_message', (data) => {
-        io.sockets.emit('new_message', { message: data.message, username: socket.username });
+        io.to(socket.user["room"]).emit('new_message', {
+          room: data.room,
+          username: socket.user["name"],  
+          message: data.message 
+        });
       });
 
       socket.on('disconnect', function () {
         console.log('user disconnected');
-        socket.broadcast.emit('disconnect_message', { message: ' has disconnected', username: socket.username});
+        socket.broadcast.to(socket.user["room"]).emit('disconnect_message', {
+          username: socket.user["name"], 
+          message: ' has disconnected', 
+        });
       });
 
       // listen on typing
       socket.on('typing', (data) => {
-        socket.broadcast.emit('typing', { typing: data.typing, username: socket.username });
+        socket.broadcast.to(socket.user["room"]).emit('typing', {
+          room: data.room, 
+          username: socket.user["name"],
+          typing: data.typing
+        });
       });
 
       socket.on('join_room', (data) => {
-        socket.broadcast.emit('leave_message', { message: ' has left the room', username: socket.username});
-        socket.join(data, () => {
-          console.log(data);
-          io.to(data).emit('join_message', { message: ' has joined the room', username: socket.username});
+        socket.leave(socket.user["room"], () => {
+          console.log(`${socket.user["name"]} left #${data.room}`);
         });
-        socket
-
-        console.log("room switched");
+        io.to(socket.user["room"]).emit('leave_message', { 
+          room: data.room,
+          username: socket.user["name"], 
+          message: ` has left the ${socket.user["room"]}`, 
+        });
+        socket.user["room"] = data.room;
+        socket.join(data.room, () => {
+          console.log(`${socket.user["name"]} joined #${data.room}`);
+          io.to(data.room).emit('join_message', { room: data.room, 
+            message: ` has joined #${data.room}`, 
+            username: socket.user["name"]
+          });
+        });
       })
 
       socket.on('leave_room', (data) => {
-        socket.leave(data, () => {
-          console.log(data);
-          io.to(data).emit('leave_message', { message: ' has joined the room', username: socket.username});
+        socket.leave(data.room, () => {
+          console.log(`${socket.user["name"]} has left #${data.room}`);
+          io.to(data.room).emit('leave_message', {
+            room: data.room,
+            username: socket.user["name"],
+            message: ` has left #${data.room}`
+          });
         });
-        console.log("room left");
       })
     });
   }
--- client/public/assets/chat.js
+++ client/public/assets/chat.js
@@ -1,108 +1,110 @@
 $(function () {
-    var socket = io.connect(`${location.protocol}//${location.hostname}:${location.port}`)
+  var socket = io.connect(`${location.protocol}//${location.hostname}:${location.port}`)
 
-    var message = $("#message");
-    var username = $("#username");
-    var send_message = $("#send_message");
-    var send_username = $("#send_username");
-    var chatroom = $("#chatroom");
-    var chatroom2 = $("#chatroom2")
-    var typing_feedback = $("#typing-feedback");
-    var connect_feedback = $('#connect-feedback');
-    var join_room = $("#join_room");
-    var leave_room = $("#leave_room")
-    var typingTimeout;
-    var connectTimeout;
-    var disconnectTimeout;
+  var message = $("#message");
+  var username = $("#username");
+  var send_message = $("#send_message");
+  var send_username = $("#send_username");
+  var chatroom = $("#chatroom");
+  var chatroom2 = $("#chatroom2")
+  var typing_feedback = $("#typing-feedback");
+  var connect_feedback = $('#connect-feedback');
+  var join_room = $(".join-room");
+  var leave_room = $("#leave_room");
+  var current_room = "main-room";
 
-    function timeoutTyping() {
-        socket.emit("typing", { typing: false });
+  var typingTimeout;
+  var connectTimeout;
+  var disconnectTimeout;
+
+  function timeoutTyping() {
+    socket.emit("typing", { typing: false, room: current_room });
+  }
+
+  function timeoutConnect() {
+    $(".connect").remove();
+  }
+
+  function timeoutDisconnect() {
+    $(".disconnect").remove();
+  }
+
+  send_message.click(() => {
+    if (message.val().length > 0)
+      socket.emit('new_message', { message: message.val(), room: current_room });
+  })
+
+  socket.on("new_message", (data) => {
+    //if (current_room == data.room)
+    chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
+  });
+
+  send_username.click(function () {
+    console.log(username.val())
+    socket.emit('change_username', { username: username.val() });
+  });
+
+  // Emit typing
+  message.bind("keypress", (e) => {
+    socket.emit('typing', { typing: true, room: current_room });
+    clearTimeout(typingTimeout);
+    typingTimeout = setTimeout(timeoutTyping, 2000);
+  });
+
+  message.on("keyup", (e) => {
+    if (e.keyCode == 13) {
+      // Submit message
+      if (message.val().length > 0) {
+        send_message.click();
+        message.val("");
+      }
     }
-    
-    function timeoutConnect() {
-        $(".connect").remove();
-    }
+  });
 
-    function timeoutDisconnect() {
-        $(".disconnect").remove();
-    }
+  // Listen on typing
+  socket.on('typing', (data) => {
+    if (data.typing)
+      typing_feedback.html("<p><em><strong>" + data.username + "</strong> is typing a message..." + "</em></p>");
+    else
+      typing_feedback.html("");
+  });
 
-    send_message.click(() => {
-        if (message.val().length > 0)
-            socket.emit('new_message', { message: message.val() });
-    })
+  socket.on('new_connection', (data) => {
+    console.log(data)
+    if (connect_feedback.find(".connect").length)
+      $(".connect").html("<strong>" + data.username + "</strong>" + data.message);
+    else
+      connect_feedback.append("<p class='connect'><strong>" + data.username + "</strong>" + data.message + "</p>");
 
-    socket.on("new_message", (data) => {
-        console.log(data)
-        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
-    });
+    clearTimeout(connectTimeout);
+    connectTimeout = setTimeout(timeoutConnect, 2000);
+  });
 
-    send_username.click(function () {
-        console.log(username.val())
-        socket.emit('change_username', { username: username.val() });
-    });
+  socket.on('disconnect_message', (data) => {
+    if (connect_feedback.find("p.disconnect").length)
+      $(".disconnect").html("<strong>" + data.username + "</strong>" + data.message);
+    else
+      connect_feedback.append("<p class='disconnect'><strong>" + data.username + "</strong>" + data.message + "</p>");
 
-    // Emit typing
-    message.bind("keypress", (e) => {
-        socket.emit('typing', { typing: true });
-        clearTimeout(typingTimeout);
-        typingTimeout = setTimeout(timeoutTyping, 2000);
-    });
+    clearTimeout(disconnectTimeout);
+    disconnectTimeout = setTimeout(timeoutDisconnect, 2000);
+  });
 
-    message.on("keyup", (e) => {
-        if (e.keyCode == 13) {
-            // Submit message
-            if (message.val().length > 0) {
-                send_message.click();
-                message.val("");
-            }
-        }
-    });
+  join_room.click((e) => {
+    current_room = e.target.id;
+    socket.emit('join_room', { room: current_room });
+    chatroom.find('.message').remove();
+  });
 
-    // Listen on typing
-    socket.on('typing', (data) => {
-        if (data.typing)
-            typing_feedback.html("<p><em><strong>" + data.username + "</strong> is typing a message..." + "</em></p>");
-        else
-            typing_feedback.html("");
-    });
+  socket.on('join_message', (data) => {
+    chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
+  })
 
-    socket.on('new_connection', (data) => {
-        console.log(data)
-        if (connect_feedback.find(".connect").length)
-            $(".connect").html("<strong>" + data.username + "</strong>" + data.message);
-        else
-            connect_feedback.append("<p class='connect'><strong>" + data.username + "</strong>" + data.message + "</p>" );
+  leave_room.click(() => {
+    socket.emit('leave_room', { room: current_room });
+  })
 
-        clearTimeout(connectTimeout);
-        connectTimeout = setTimeout(timeoutConnect, 2000);
-    });
-
-    socket.on('disconnect_message', (data) => {
-        if (connect_feedback.find("p.disconnect").length)
-            $(".disconnect").html("<strong>" + data.username + "</strong>" + data.message);
-        else
-            connect_feedback.append("<p class='disconnect'><strong>" + data.username + "</strong>" + data.message + "</p>");
-
-        clearTimeout(disconnectTimeout);
-        disconnectTimeout = setTimeout(timeoutDisconnect, 2000);
-    });
-
-    join_room.click(function(){
-        socket.emit('join_room', 'room1');
-    })
-
-    socket.on('join_message', (data) => {
-        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
-        console.log(data)
-    })
-
-    leave_room.click(function(){
-        socket.emit('leave_room', 'room1')
-    })
-
-    socket.on('leave_message', (data) => {
-        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
-        console.log(data)
-    })
+  socket.on('leave_message', (data) => {
+    chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>");
+  })
 });
