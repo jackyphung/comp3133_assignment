95746f2bcdaff10ccd5a6d9cdb68af0460b41f35
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-03-07 11:45:11 -0500

    Updated History to not include date creation time

--- backend/models/History.js
+++ backend/models/History.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const HistorySchema = new Schema({
   room_name: String,
-  date_created: Date,
   chat_history = new Schema({
     username: String,
     message: String,
