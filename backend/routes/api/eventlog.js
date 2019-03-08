198f1e07d1039b0e1f6a3f87e28b7e7663bf8a0a
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-03-08 17:46:59 -0500

    Updated code formatting

--- backend/routes/api/eventlog.js
+++ backend/routes/api/eventlog.js
@@ -4,18 +4,18 @@ const bodyParser = require('body-parser')
 
 const EventLog = require('../../models/EventLog')
 
-router.use((req,res,next) => {
-    next();
+router.use((req, res, next) => {
+  next();
 })
 
 router.get('', (req, res, next) => {
-    res.contentType('application/json');
-    console.log('get event log')
-    EventLog.find({}, (err, elog) => {
-        if(err)
-            throw err;
-        res.send(JSON.stringify(elog));
-    });
+  res.contentType('application/json');
+  console.log('get event log')
+  EventLog.find({}, (err, elog) => {
+    if (err)
+      throw err;
+    res.send(JSON.stringify(elog));
+  });
 })
 
 module.exports = router;
--- backend/routes/api/history.js
+++ backend/routes/api/history.js
@@ -4,32 +4,32 @@ const bodyParser = require('body-parser')
 
 const History = require('../../models/History')
 
-router.use((req,res,next) => {
-    next();
+router.use((req, res, next) => {
+  next();
 })
 
 router.get('', (req, res, next) => {
-    res.contentType('application/json');
-    console.log('get history list')
-    History.find({}, (err, hist) => {
-        if(err)
-            throw err;
-        res.send(JSON.stringify(hist));
-    });
+  res.contentType('application/json');
+  console.log('get history list')
+  History.find({}, (err, hist) => {
+    if (err)
+      throw err;
+    res.send(JSON.stringify(hist));
+  });
 })
 
 router.get('/:room', (req, res, next) => {
-    res.contentType('application/json');
-    if(req.params.room) {
-        console.log('get room history list')
-        History.findOne({room: req.params.room}, (err, hist) => {
-            if(err)
-                throw err
-            if(hist == null) 
-                res.send(JSON.stringify({ message: `The user "${req.params.room_name}" does not exist in the database` }));
-            res.send(JSON.stringify(hist))
-        })
-    }
+  res.contentType('application/json');
+  if (req.params.room) {
+    console.log('get room history list')
+    History.findOne({ room: req.params.room }, (err, hist) => {
+      if (err)
+        throw err
+      if (hist == null)
+        res.send(JSON.stringify({ message: `The user "${req.params.room_name}" does not exist in the database` }));
+      res.send(JSON.stringify(hist))
+    })
+  }
 })
 
 module.exports = router;
