7a650b772462a2f3c828c957aa12b3fdd98bea3f
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-03-07 20:48:45 -0500

    [Models] Fixed Chat History subdocument schema declaration in History

--- backend/models/History.js
+++ backend/models/History.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const HistorySchema = new Schema({
   room: String,
-  chat_history = new Schema({
+  chat_history: [new Schema({
     username: String,
     message: String,
     time: Date
-  })
+  })]
 });
 
-module.exports = mongoose.model('user_history', UserHistorySchema);
+module.exports = mongoose.model('room_history', HistorySchema);
