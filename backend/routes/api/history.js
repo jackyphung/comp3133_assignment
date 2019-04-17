04d08dd2cd9a520fb6d5d662f2026fe11cbd393d
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-04-17 18:10:42 -0400

    [Server][History] Add create, and update routes

--- backend/routes/api/history.js
+++ backend/routes/api/history.js
@@ -21,13 +21,50 @@ router.get('', (req, res, next) => {
 router.post('', (req, res, next) => {
   res.contentType('application/json');
   if (req.body.roomname) {
-    console.log('get room history list')
+    console.log('get list of rooms')
     History.findOne({ room: req.body.roomname }, (err, hist) => {
       if (err)
         throw err
       if (hist == null)
-        res.send(JSON.stringify({ message: `The user "${req.params.room_name}" does not exist in the database` }));
-      res.send(JSON.stringify(hist))
+        res.send(JSON.stringify({ message: `The room "${req.body.roomname}" does not exist in the database` }));
+      else 
+        res.send(JSON.stringify(hist))
+    })
+  } else {
+    res.send(JSON.stringify({ message: "Invalid Room History Request" }));
+  }
+})
+
+router.post('create', (req, res, next) => {
+  res.contentType('application/json');
+  if (req.body.roomname) {
+    console.log('create room')
+    let history = History(req.body.roomname);
+    history.save((err, hist) => {
+      if (err) 
+        throw err;
+
+      if (hist == null)
+        res.send(JSON.stringify({ message: `The room "${req.body.roomname}" has not been created.` }));
+      else
+        res.send(JSON.stringify(hist))
+    })
+  } else {
+    res.send(JSON.stringify({ message: "Invalid Room History Request" }));
+  }
+})
+
+router.post('update', (req, res, next) => {
+  res.contentType('application/json');
+  let values = req.body;
+  if (values.roomname) {
+    History.findOneAndUpdate({ room: values.roomname }, { $set: values }, { new: true }, (err, hist) => {
+      if (err)
+        throw err
+      if (hist == null)
+        res.send(JSON.stringify({ message: `The room "${values.roomname}" does not exist in the database` }));
+      else 
+        res.send(JSON.stringify(hist))
     })
   } else {
     res.send(JSON.stringify({ message: "Invalid Room History Request" }));
