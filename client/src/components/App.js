ab65e2541ac5132b3ce2f69214a6e189331f405c
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-04-17 09:50:27 -0400

    [Client] Commented out old modal

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import { Home, NotFound } from 'Pages';
+import 'assets/css/App.css';
+import 'assets/css/Animations.css';
+import 'assets/css/General.css';
+import 'assets/css/Users.css';
 import { Switch, Route } from 'react-router-dom';
+import { ContentBody, ContentArea, ContentBlock } from 'Layout';
+import { Home, NotFound } from 'Pages';
 
 class App extends Component {
   componentDidMount() { }
@@ -8,13 +13,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>We have something!</h1>
-        <Switch>
-          <Route path="/" render={(props) => <Home {...props}/>}/>
-          <Route path="/room/:roomId" render={(props) => <Home {...props}/>}/>
-          <Route path="/admin" component={NotFound}/> {/* Placeholder for Administrators... if we get to it */}
-          <Route path="*" component={NotFound}/>
-        </Switch>
+        <ContentBody>
+          <Switch>
+            <Route path="/" render={(props) => <Home {...props}/>}/>
+            <Route path="/room/:roomId" render={(props) => <Home {...props}/>}/>
+            <Route path="/admin" component={NotFound}/> {/* Placeholder for Administrators... if we get to it */}
+            <Route path="*" component={NotFound}/>
+          </Switch>
+        </ContentBody>
+        {/* <div id="rooms" className="modal show">
+					<div className="modal-content">
+						<div className="modal-header">
+							Super Chat Room List
+							<h6>Please select a room:</h6>
+						</div>
+						<div className="modal-body">
+								<div id="main-room" className="join-room" type="button">main-room</div>
+								<div id="alt-room" className="join-room" type="button">alt-room</div>
+						</div>
+					</div>
+				</div> */}
       </div>
     );
   }
