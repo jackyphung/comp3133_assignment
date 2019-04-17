5592fb58dcddf43eb4a626311b2e624caf47fa95
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-04-17 17:08:13 -0400

    Updated scripts and webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 // webpack.config.js
-const { resolve } = require('path');
+const { resolve, join } = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin')
@@ -9,11 +9,11 @@ module.exports = {
   entry: {
     'bundle': [
       'babel-polyfill',
-      './client/src/index.js',
+      resolve(__dirname, './client/src/index.js'),
     ],
     "bundle.min": [
       'babel-polyfill',
-      './client/src/index.js',
+      resolve(__dirname, './client/src/index.js'),
     ]
   },
   output: {
@@ -68,4 +68,30 @@ module.exports = {
       },
     }),]
   },
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    inline: true,
+    overlay: true,
+    contentBase: resolve(__dirname, './client/public/'), //serve your static files from here
+    watchContentBase: true,
+    proxy: [ // allows redirect of requests to webpack-dev-server to another destination
+      {
+        context: [ 
+          '/api/**', 
+        ],  // can have multiple
+        target: 'http://localhost:8000', //server and port to redirect to
+        secure: false //don't use https
+      }
+    ],
+    port: 3030, // port webpack-dev-server listens to, defaults to 8080
+    overlay: { // Shows a full-screen overlay in the browser when there are compiler errors or warnings
+      warnings: false, // defaults to false
+      errors: false, // defaults to false
+    },
+    historyApiFallback: {
+      true: true,
+      index: "index.html"
+    },
+  }
 }
--- webpack.prod.js
+++
@@ -1,7 +0,0 @@
-const { resolve } = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.config.js');
-
-module.exports = (common, {
-  mode: 'production',
-});
