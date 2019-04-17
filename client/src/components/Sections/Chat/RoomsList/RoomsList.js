c8960661165db0b239c6cf7711352a507800c350
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-04-17 09:51:41 -0400

    [Client] Started creating chat components

---
+++ client/src/components/Sections/Chat/RoomsList/RoomsList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter} from 'Layout';
+import classnames from 'classnames';
+import './RoomsList.js';
+
+class RoomsList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {  
+
+  }
+
+  render() { 
+    const { id, className, contentClassName, children, style, show, toggle, fade } = this.props;
+    let classNames = classnames(
+      className, fade ? "fade-in" : ``
+    );
+
+    return (  
+      <Modal id={"rooms"} show={show} toggle={toggle}
+        className={classNames} contentClassName={contentClassName}>
+        <ModalHeader>
+          <h2>Super Chat Room List</h2>
+          <h4>Please select a room:</h4>
+        </ModalHeader>
+        <ModalBody className="d-flex">
+          Usually there would be rooms here... but for now there isn't.
+        </ModalBody>
+      </Modal>
+    );
+  }
+}
+ 
+export { RoomsList };
