435d5b1f3f908a6196e54f2269a33e043c9a6a49
Jullian Anthony Sy-Lucero <jsyluc123@hotmail.com>
2019-04-18 10:47:03 -0400

    [Client] Socket.IO function restored to Username

--- client/src/components/Sections/Chat/ChatRoom/ChangeUsername/ChangeUsername.js
+++ client/src/components/Sections/Chat/ChatRoom/ChangeUsername/ChangeUsername.js
@@ -16,14 +16,20 @@ class ChangeUsername extends Component {
     e.preventDefault();
     const { setUsername } = this.props;
     setUsername({ username: e.target["chat-username"].value }, () => {
-      socket.emit('change_username', { username: e.target["chat-username"].value })
+      console.log(`Username was set to ${this.state.username}!`);
+      socket.emit('change_username', { username: this.state.username })
     })
   }
 
+  handleInput = (e) => {
+    this.setState({ username: e.target.value.trim() });
+  }
+
   render() {
+    const { username } = this.state;
     return (
       <form className="change-username d-flex" onSubmit={this.changeUsername}>
-        <input className="username" name="chat-username" type="text" placeholder="Username"/>
+        <input className="username" name="chat-username" type="text" placeholder="Username" value={username} onChange={this.handleInput}/>
         <button className="send-username" type="submit">Set Username</button>
       </form>
     );
